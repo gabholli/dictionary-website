893c629f4f8e88802c59cc1fe2c6613149c99ca3
Gabe Hollifield <gholl467@gmail.com>
2025-10-03 12:29:00 -0700

    Altered placeholder text in text input field

--- src/app/components/SearchForm.tsx
+++ src/app/components/SearchForm.tsx
@@ -1,7 +1,7 @@
 export default function SearchForm({ handleSubmit }: { handleSubmit: (e: React.FormEvent<HTMLFormElement>) => void, search: string }) {
     return (
         <form className="mb-4 flex justify-center items-center gap-x-4" onSubmit={handleSubmit}>
-            <input className="bg-blue-100 py-2 indent-4" name="search" type="text" placeholder="Search for a word..." />
+            <input className="bg-blue-100 py-2 indent-4" name="search" type="text" placeholder="Search for a word" />
             <button className="bg-black text-white px-4 py-2 cursor-pointer hover:text-black hover:bg-blue-100" type="submit">Search</button>
         </form>
     )
