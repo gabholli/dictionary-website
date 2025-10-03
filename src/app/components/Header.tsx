c18d69a0c913620a11027ed43abd657c68803aae
Gabe Hollifield <gholl467@gmail.com>
2025-10-03 12:38:06 -0700

    Updated title text sizes for different screens

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,6 +1,7 @@
 export default function Header() {
     return (
-        <div className="flex justify-center items-center p-4 border-b border-gray-300">
+        <div className="flex justify-center items-center p-4 border-b border-gray-300 md:text-2xl
+        lg:text-3xl xl:text-4xl font-bold 2xl:text-5xl">
             <h1>Free Dictionary</h1>
         </div>
 
